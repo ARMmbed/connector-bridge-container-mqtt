11d2fbe02aba589d2443618efa814e1434d79756
Doug Anson <doug.anson@arm.com>
2016-08-12 10:17:21 -0500

    updated

--- run-reload-bridge.sh
+++ run-reload-bridge.sh
@@ -7,7 +7,6 @@ ID=`docker ps | grep connector-bridge | awk '{print $1}'`
 if [ "${ID}X" != "X" ]; then
     echo "Stopping $ID"
     docker stop ${ID}
-    sleep 30
 else
     echo "No running bridge container found... OK"
 fi
@@ -15,7 +14,6 @@ fi
 if [ "${ID}X" != "X" ]; then
     echo "Removing $ID"
     docker rm --force ${ID}
-    sleep 30
 fi
 
 echo "Looking for existing container image..."
@@ -24,7 +22,6 @@ ID=`docker images | grep connector-bridge | awk '{print $3}'`
 if [ "${ID}X" != "X" ]; then
     echo "Removing Image $ID"
     docker rmi --force ${ID}
-    sleep 30
 else
     echo "No container image found... (OK)"
 fi
@@ -37,4 +34,16 @@ if [ -f Dockerfile ]; then
 else
     echo "Unable to find Dockerfile... change to repo directory and retry..."
     exit 1
+fi
+
+if [ "$?" = "0" ]; then
+    echo "Starting Bridge..."
+    ./start-bridge.sh
+    if [ "$?" = "0" ]; then
+        echo "Bridge Started!"
+        exit 0
+    else
+ 	echo "Bridge Start FAILED"
+	exit 2
+    fi
 fi
